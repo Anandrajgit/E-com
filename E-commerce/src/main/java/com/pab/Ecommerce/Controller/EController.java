e7ebf3b20aa3240b210510f2a23031babd1e0556
sys-user <sys-user@SMI-203>
2022-10-17 12:20:56 +0530

    Jsp home page has been included

--- E-commerce/src/main/java/com/pab/Ecommerce/Controller/EController.java
+++ E-commerce/src/main/java/com/pab/Ecommerce/Controller/EController.java
@@ -1,29 +1,31 @@
 package com.pab.Ecommerce.Controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class EController {
 
 	    @GetMapping("/")
 	    public String home() {
-	        return ("<h1>Welcome</h1>");
+	        return ("HomePage");
 	    }
 
-	    @GetMapping("/customer")
-	    public String customer() {
-	        return ("<h1>Welcome customer</h1>");
+//	    @GetMapping("/customer")
+//	    public String customer() {
+//	        return ("Homepage");
+//	    }
+//
+//	    @GetMapping("/seller")
+//	    public String seller() {
+//	        return ("Homepage");
+//	    }
+//	    @GetMapping("/admin")
+//	    public String admin() {
+//	        return ("Homepage");
 	    }
-
-	    @GetMapping("/seller")
-	    public String seller() {
-	        return ("<h1>Welcome seller</h1>");
-	    }
-	    @GetMapping("/admin")
-	    public String admin() {
-	        return ("<h1>Welcome Admin</h1>");
-	    }
-	}
+	
 
 
 
